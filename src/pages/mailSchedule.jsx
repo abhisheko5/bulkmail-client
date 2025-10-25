544cf7f3eb29583fe3301613dab42c924e31e199
abhisheko5 <86157818+abhisheko5@users.noreply.github.com>
2025-10-25 15:36:32 +0530

    added queue

--- src/pages/mailSchedule.jsx
+++ src/pages/mailSchedule.jsx
@@ -260,7 +260,6 @@ export default function CreateCampaign() {
                   Schedule Time
                 </label>
                 <input
-                  type="datetime-local"
                   className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
                 />
                 <p className="text-sm text-gray-500 mt-2">
